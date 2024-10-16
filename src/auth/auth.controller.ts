38139c1d6c488d5b83250ad38a17445aa6dc1338
zaid <zaidzued@gmail.com>
2024-10-16 17:11:39 +0700

    update api auth sign in/up with conseption DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from "./auth.service";
+import { SignUpDto } from './dto/sign_up.dto';
+import { SignInDto } from './dto/sign_in.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService : AuthService){}
 
-    @Post('/signIn')
-    async signIn(@Body() signData : { username:string, password:string }) {
-        return this.authService.handleSignIn(signData)
+    @Post('/sign_in')
+    async signIn(@Body() signInData : SignInDto) {
+        return this.authService.handleSignIn(signInData)
+    }
+
+    @Post('/sign_up')
+    async signUp(@Body() signUpData : SignUpDto) {
+        return this.authService.handleSignUp(signUpData)
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,31 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { SignUpDto } from './dto/sign_up.dto';
+import { SignInDto } from './dto/sign_in.dto';
 
 @Injectable()
 export class AuthService {
-    handleSignIn(signData: { username: string; password: string; }):any {
-        const { username, password } = signData
-
-        if(!username || !password) throw new BadRequestException('username or password is empty')
+    handleSignIn(signInData: SignInDto):object {
+        const { username, password } = signInData
+        console.log(signInData)
 
         return {
             status: 'succeed',
             status_code : 200,
             message : 'Congratulations, you have successfully logged in.'
+        }
+    }
+
+    handleSignUp(signUpData: SignUpDto):object {
+        const { username, password, confirm_password, email, address, date_of_birth, no_phone } = signUpData
+
+        if(password != confirm_password) throw new BadRequestException('sorry password and password confirmation are not the same.')
+
+        console.log(signUpData);
+
+        return {
+            status: 'succeed',
+            status_code : 200,
+            message : 'Congratulations, you have successfully sign up.'
         }
     }
 }
